90d503a0575a7d4a2b231ab906257621d7059df4
NikolasMoores <nikolas.moores@my.jcu.edu.au>
2021-03-20 21:52:08 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,20 +1,30 @@
+def main():
 
-min_length = 2
-
-
-print("Enter Password")
-
-valid = False
-
-while valid == False:
-    password = input("> ")
-    if len(password) < min_length:
-        print("Invalid Length")
-    else:
-        asterisk = '*'
-        for i in range(0, len(password)-1, 1):
-            asterisk = asterisk+'*'
-        print(asterisk)
-        valid = True
+    min_length = 2
 
 
+    print("Enter Password")
+
+    password = get_password(min_length)
+    asterisk = print_asterisk(password)
+    print(asterisk)
+
+def print_asterisk(password):
+    asterisk = '*'
+    for i in range(0, len(password) - 1, 1):
+        asterisk = asterisk + '*'
+    return asterisk
+
+
+def get_password(min_length):
+    valid = False
+    while valid == False:
+        password = input("> ")
+        if len(password) < min_length:
+            print("Invalid Length")
+        else:
+            valid = True
+    return password
+
+
+main()
